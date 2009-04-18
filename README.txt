2c32e2686a91554c9aa2fda801d91d66212f98e4
Thomas Balthazar <thomas@suitmymind.com>
2009-04-18 20:18:59 +0200

    readme updated

--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
 Created by Thomas Balthazar, Copyright 2009
 This script is provided as is, and is released under the MIT license : http://www.opensource.org/licenses/mit-license.php
+more information here : http://suitmymind.com/2009/04/18/move-your-tickets-from-lighthouse-to-github/
--- migrate-lh-to-gh.rb
+++ migrate-lh-to-gh.rb
@@ -1,5 +1,6 @@
 # Created by Thomas Balthazar, Copyright 2009
 # This script is provided as is, and is released under the MIT license : http://www.opensource.org/licenses/mit-license.php
+# more information here : http://suitmymind.com/2009/04/18/move-your-tickets-from-lighthouse-to-github/
 
 require 'rubygems'
 require 'lighthouse-api'
