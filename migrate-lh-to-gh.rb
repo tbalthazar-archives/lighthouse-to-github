19c2867641373c743be44ca28b8f61c9b6755e8d
Thomas Balthazar <thomas@suitmymind.com>
2009-04-21 14:44:32 +0200

    bug fix, improvements and comment creation. Closes #3 Closes #4 Closes #5 Closes #6

--- migrate-lh-to-gh.rb
+++ migrate-lh-to-gh.rb
@@ -21,9 +21,11 @@ GITHUB_LOGIN      = "YOUR_ACCOUNT_NAME"
 GITHUB_API_TOKEN  = "YOUR_API_TOKEN"
 GITHUB_PROJECT    = "YOUR_GITHUB_PROJECT_NAME"
 
+
 # do not modify
 GITHUB_NEW_ISSUE_API_URL    = "https://github.com/api/v2/yaml/issues/open/#{GITHUB_LOGIN}/#{GITHUB_PROJECT}"
 GITHUB_ADD_LABEL_API_URL    = "https://github.com/api/v2/yaml/issues/label/add/#{GITHUB_LOGIN}/#{GITHUB_PROJECT}"
+GITHUB_ADD_COMMENT_API_URL  = "https://github.com/api/v2/yaml/issues/comment/#{GITHUB_LOGIN}/#{GITHUB_PROJECT}"
 
 
 # -----------------------------------------------------------------------------------------------
@@ -52,47 +54,57 @@ tickets.each { |ticket|
   # fetch the ticket individually to have the different 'versions'
   ticket = Lighthouse::Ticket.find(ticket.id, :params => { :project_id => LIGHTHOUSE_PROJECT_ID})
   
-  title = ticket.title
-  body  = ""
-
   # get the ticket versions/history
-  ticket.versions.each { |version|
-    # create a title for each new ticket history/version
-    unless version.title==ticket.versions.first.title && version.body==ticket.versions.first.body
-      body+="\n\n**#{version.title}**\n" 
-      version.title.length.times do |i|
-        body+="-"
-      end
-      body+="\n"
-    end
-    # gsub : curl -F 'body=@xxx' -> 'body= @xxx' cause =@xxx means xxx is a file to upload http://curl.haxx.se/docs/manpage.html#-F--form
-    body+=version.body.gsub(/^@/," @") unless version.body.nil? 
-  }
+  versions = ticket.versions  
+  
+  # this is the assigned user name of the corresponding LH ticket  
+  assignee = versions.last.assigned_user_name unless versions.last.attributes["assigned_user_name"].nil?  
+
+  # why gsub? -> curl -F 'title=@xxx' -> 'title= @xxx' cause =@xxx means xxx is a file to upload http://curl.haxx.se/docs/manpage.html#-F--form
+  title = ticket.title.gsub(/^@/," @")
+  body  = versions.first.body.gsub(/^@/," @") unless versions.first.body.nil? 
+  body||=""
   
   # add the original LH ticket URL at the end of the body
   body+="\n\n[original LH ticket](#{ticket.url})"
   
+  # add the number of attachments
+  body+="\n\n This ticket has #{ticket.attachments_count} attachment(s)." unless ticket.attributes["attachments_count"].nil?
+
   # escape single quote
   title.gsub!(/'/,"&rsquo;")
-  title.gsub!(/^@/," @")
   body.gsub!(/'/,"&rsquo;")
-  
+
+  # the first version contains the initial ticket body
+  versions.delete_at(0) 
+    
   # create the GH issue and get its newly created id
   gh_return_value = `curl -F 'login=#{GITHUB_LOGIN}' -F 'token=#{GITHUB_API_TOKEN}' -F 'title=#{title}' -F 'body=#{body}' #{GITHUB_NEW_ISSUE_API_URL}`
   gh_issue_id = YAML::load(gh_return_value)["issue"]["number"]
+  
+  # add comments to the newly created GH issue
+  versions.each { |version|
+    # add the LH comment title to the comment
+    comment = "**#{version.title.gsub(/^@/," @").gsub(/'/,"&rsquo;")}**\n\n"
+    comment+=version.body.gsub(/^@/," @").gsub(/'/,"&rsquo;") unless version.body.nil?
+    comment+="\n\n by " + version.user_name.gsub(/^@/," @").gsub(/'/,"&rsquo;") unless version.user_name.nil?
+    `curl -F 'login=#{GITHUB_LOGIN}' -F 'token=#{GITHUB_API_TOKEN}' -F 'comment=#{comment}' #{GITHUB_ADD_COMMENT_API_URL}/#{gh_issue_id}`
+  }  
   
   # here you can specify the labels you want to be applied to your newly created GH issue
   # preapare the labels for the GH issue
   gh_labels = []
   # these are the tags of the corresponding LH ticket, replace @ by # because @ will be used to tag assignees in GH
-  gh_labels += ticket.tags.map { |tag| tag.gsub("@","#") }  
-  gh_labels << ticket.milestone_title if ticket.responds_to?(:milestone_title) # this is the milestone title of the corresponding LH ticket
-  gh_labels << "@" + ticket.versions.last.assigned_user_name unless ticket.versions.last.attributes["assigned_user_name"].nil?  # this is the assigned user name of the corresponding LH ticket
-  gh_labels << ticket.state # this is the state of the corresponding LH ticket
+  gh_labels += ticket.tags.map { |tag| tag.gsub(/^@/,"#") }  
+  gh_labels << "|M| " + ticket.milestone_title unless ticket.attributes["milestone_title"].nil? # this is the milestone title of the corresponding LH ticket
+  gh_labels << "@" + assignee unless assignee.nil?
+  gh_labels << "|S| " + ticket.state # this is the state of the corresponding LH ticket
   gh_labels << "from-lighthouse" # this is a label that specify that this GH issue has been created from a LH ticket
-    
+  
   # tag the issue
   gh_labels.each { |label|
+    # labels containing . do not work ... -> replace . by •
+    label.gsub!(/\./,"•")
     `curl -F 'login=#{GITHUB_LOGIN}' -F 'token=#{GITHUB_API_TOKEN}' #{GITHUB_ADD_LABEL_API_URL}/#{URI.escape(label)}/#{gh_issue_id}`
   }
 }
